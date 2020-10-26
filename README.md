3660409c710cd3e1c9b78ba7d33a28ec508b4717
wgj6112345 <2272499332@qq.com>
2020-10-26 21:23:50 +0800

    update...

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
 # go_crontab
+
+基于 etcd 实现的分布式定时任务
+
+## 使用方法
+
+```
+# 启动 master 节点
+
+cd ./master/main
+go build
+
+
+# 启动 server 之前 请先开启 etcd 和 mongo 的服务
+main.exe
+
+# 管理界面在 http://localhost:10020
+
+
+
+# 启动 worker 节点
+
+cd ./worker/main
+go build
+
+main.exe
+
+
+
+
+
+```
--- master/main/main.go
+++ master/main/main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"flag"
 	"fmt"
-	"runtime"
 
 	"github.com/wgj6112345/go-crontab/go_crontab/master"
 )
@@ -11,10 +10,6 @@ import (
 var (
 	configFile string
 )
-
-func initEnv() {
-	runtime.GOMAXPROCS(runtime.NumCPU())
-}
 
 func initArgs() {
 	flag.StringVar(&configFile, "f", "./master.json", "specify config file")
@@ -28,25 +23,17 @@ func main() {
 	// 初始化命令行参数
 	initArgs()
 
-	// 初始化线程
-	initEnv()
-
 	// 加载配置
 	if err = master.InitConfig(configFile); err != nil {
 		panic(err)
 	}
-
-	// 启动日志模块
 	if err = master.InitLogMgr(); err != nil {
 		panic(err)
 	}
-
-	// 启动任务管理器
 	if err = master.InitJobMgr(); err != nil {
 		fmt.Println(err)
 	}
-
-	// 启动 worker 节点检查
+	// worker 节点检查
 	if err = master.InitWorkerMgr(); err != nil {
 		fmt.Println(err)
 	}
@@ -56,7 +43,7 @@ func main() {
 		panic(err)
 	}
 
-	//
+	// 阻塞
 	for {
 	}
 }
